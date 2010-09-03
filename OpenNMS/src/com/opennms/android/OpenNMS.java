263a7994e7db1c1bd4f90f4ba1005d4ef7635e88
Benjamin Reed <rangerrick@befunk.com>
2010-09-03 14:04:52 -0400

    mock data for the outage list

--- OpenNMS/src/com/opennms/android/OpenNMS.java
+++ OpenNMS/src/com/opennms/android/OpenNMS.java
@@ -20,7 +20,7 @@ public class OpenNMS extends TabActivity {
 
         Resources res = getResources(); // Resource object to get Drawables
         TabHost tabHost = getTabHost();  // The activity TabHost
-        TabHost.TabSpec spec;  // Resusable TabSpec for each tab
+        TabHost.TabSpec spec;  // Reusable TabSpec for each tab
         Intent intent;  // Reusable Intent for each tab
 
         // Create an Intent to launch an Activity for the tab (to be reused)
--- OpenNMS/src/com/opennms/android/outages/OutageActivity.java
+++ OpenNMS/src/com/opennms/android/outages/OutageActivity.java
@@ -1,16 +1,37 @@
 package com.opennms.android.outages;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class OutageActivity extends Activity {
+import com.opennms.android.R;
+
+public class OutageActivity extends ListActivity {
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        TextView textview = new TextView(this);
-        textview.setText("Outage List Here");
-        setContentView(textview);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.severity_item, TESTDATA));
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
+
+        lv.setOnItemClickListener(new OnItemClickListener() {
+          public void onItemClick(AdapterView<?> parent, View view,
+              int position, long id) {
+            // When clicked, show a toast with the TextView text
+            Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+                Toast.LENGTH_SHORT).show();
+          }
+        });
     }
 
+    public static String[] TESTDATA = new String[] {
+    	"Outage 1",
+    	"Outage 2"
+    };
 }
